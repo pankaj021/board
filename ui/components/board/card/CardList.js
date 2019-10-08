f647e656d1b32461e374401b6ac6f8ae99a84a7c
pankaj021 <kssm.pankaj@gmail.com>
2019-10-08 15:55:39 +0530

    update card for all user fix

--- ui/components/board/card/CardList.js
+++ ui/components/board/card/CardList.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import shortid from 'shortid';
 import Card from './Card';
 import ColumnInput from '../column/ColumnInput';
 
+let randomIdGenerator = () => (shortid.generate() + Math.ceil(100000000 * Math.random()));
+
 let CardList = ( props ) => {
     const headerCol = props.headerCol;
+
     return <div className='Card-list'>
         {
             props.cards.map( card => {
@@ -12,7 +16,7 @@ let CardList = ( props ) => {
                         <ColumnInput {...card} btnText="Update" addedBy={card.addedBy || 'All'} />
                     </div>
                 )
-                return <Card key={card._id} card={card} headerCol={headerCol}/>
+                return <Card key={randomIdGenerator()} card={card} headerCol={headerCol}/>
             })
         }
     </div>
